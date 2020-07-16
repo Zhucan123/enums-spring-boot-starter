20daa73ef6a3499cc1b9c311ef275c299c1c9ac5
zhucan <956103955@qq.com>
2020-07-16 16:31:21 +0800

    feat : Initializer conversion

---
+++ jpa-conversion/src/main/java/com/zhucan/jpa/conversion/ConverterPackageScan.java
@@ -0,0 +1,14 @@
+package com.zhucan.jpa.conversion;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+/**
+ * @author: zhuCan
+ * @date: 2020/7/13 13:59
+ * @description:
+ */
+@EntityScan(basePackages = "com.zhucan.jpa.conversion.converter")
+public class ConverterPackageScan {
+
+
+}
---
+++ jpa-conversion/src/main/java/com/zhucan/jpa/conversion/converter/CharListConverter.java
@@ -0,0 +1,13 @@
+package com.zhucan.jpa.conversion.converter;
+
+import javax.persistence.Converter;
+import java.util.List;
+
+/**
+ * @author: zhuCan
+ * @date: 2020/7/10 18:12
+ * @description:
+ */
+@Converter(autoApply = true)
+public class CharListConverter extends AbstractJsonConverter<List<Character>> {
+}
