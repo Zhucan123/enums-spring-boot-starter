0e9a0aab076eb94a338d8effdffbc9919f99081c
朱灿 <zhuc01@mingyuanyun.com>
2022-09-14 21:08:49 +0800

    perf: conversion 修复entityScan扫描基础包路径
    1. 优化core引用
    2. 抽离Core核心module

--- enum-conversion/src/main/java/com/zhucan/enums/conversion/ConverterPackageScan.java
+++ enum-conversion/src/main/java/com/zhucan/enums/conversion/ConverterPackageScan.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
  * @date: 2020/7/13 13:59
  * @description: 用于扫描默认的内置基础转换器, 加载进JPA的转换配置
  */
-@EntityScan(basePackages = "com.zhucan.conversion.converter")
+@EntityScan(basePackages = "com.zhucan.enums.conversion.converter")
 public class ConverterPackageScan {
 
 }
